74ffa5d5ddea5c79e1fc0ba1591b17339db0461d
Hunter Danielson <hunterleedanielson@gmail.com>
2020-05-10 19:31:38 -0700

    updating components

---
+++ src/Components/Contact.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+
+    if(this.props.data){
+      var name = this.props.data.name;
+      var street = this.props.data.address.street;
+      var city = this.props.data.address.city;
+      var state = this.props.data.address.state;
+      var zip = this.props.data.address.zip;
+      var phone= this.props.data.phone;
+      var email = this.props.data.email;
+      var message = this.props.data.contactmessage;
+    }
+
+    return (
+      <section id="contact">
+
+         <div className="row section-head">
+
+            <div className="two columns header-col">
+
+               <h1><span>Get In Touch.</span></h1>
+
+            </div>
+
+            <div>
+
+                  <p className="lead">{message}</p>
+
+            </div>
+            <aside>
+               <div>
+
+					   <h4>Conctact Info</h4>
+					   <p className="address">
+						   {name}<br />
+						   {street} <br />
+						   {city}, {state} {zip}<br />
+						   <span>{phone}</span><br />
+                     {email}<br />
+					   </p>
+				   </div>
+
+            </aside>
+
+         </div>
+
+
+
+            
+   </section>
+    );
+  }
+}
+
+export default Contact;
