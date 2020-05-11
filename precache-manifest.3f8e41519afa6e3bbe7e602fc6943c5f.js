4d16afbd104d86f0077c5b65003ea0d6c49813e6
Hunter Danielson <hunterleedanielson@gmail.com>
2020-05-10 22:39:53 -0700

    Updates

---
+++ precache-manifest.3f8e41519afa6e3bbe7e602fc6943c5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64289ba34f67d0affde50c48a0fea9f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5c7eb2086143504b0f01",
+    "url": "/portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "a687df4c8ac6df8b5246",
+    "url": "/portfolio/static/js/2.1b4dfd3c.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/portfolio/static/js/2.1b4dfd3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c7eb2086143504b0f01",
+    "url": "/portfolio/static/js/main.91723321.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  }
+]);
