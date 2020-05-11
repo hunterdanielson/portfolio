1ebd4fb7c141d0061aee22da525bdc710c101f17
Hunter Danielson <hunterleedanielson@gmail.com>
2020-05-10 22:55:31 -0700

    Updates

---
+++ precache-manifest.90ba1b4ccb27f70b35bfc89d01fd9a13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ac81585e5880c955df58937eb604b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a1c0b20800d24bf4484",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "a687df4c8ac6df8b5246",
+    "url": "/static/js/2.1b4dfd3c.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.1b4dfd3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a1c0b20800d24bf4484",
+    "url": "/static/js/main.ba906f80.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3f8e41519afa6e3bbe7e602fc6943c5f.js"
+  "/precache-manifest.90ba1b4ccb27f70b35bfc89d01fd9a13.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
