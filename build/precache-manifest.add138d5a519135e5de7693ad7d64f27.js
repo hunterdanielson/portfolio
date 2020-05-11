6d5c604cf300333fc2db9385bb1e3f0789c7b41a
Hunter Danielson <hunterleedanielson@gmail.com>
2020-05-10 21:56:24 -0700

    adding build

---
+++ build/precache-manifest.add138d5a519135e5de7693ad7d64f27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad6918a05e9ba220e756111b0aaec28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ee86ed93f00fad4696d",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "2c32935546528998147d",
+    "url": "/static/js/2.160baab3.chunk.js"
+  },
+  {
+    "revision": "4ee86ed93f00fad4696d",
+    "url": "/static/js/main.d821277f.chunk.js"
+  },
+  {
+    "revision": "0ac2b0fd5fd4af3be109",
+    "url": "/static/js/runtime-main.8896086d.js"
+  }
+]);
